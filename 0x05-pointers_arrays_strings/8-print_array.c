d5b09b13d59f1b7772149bba251036e2d551e9c3
evanceondego <ondegoevance@gmail.com>
2023-02-27 12:56:21 -0800

    8-print_array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements in the array
+ *
+ * @a: array to print
+ * @n: no.of elements to print
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+
+		printf("%d", a[i]);
+
+		if (i < (n - 1))
+
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+}
+
