84a41cc520258e57ab0a690c14e23f3e5f8dadee
evanceondego <ondegoevance@gmail.com>
2023-02-27 12:02:58 -0800

    0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a func that takes a pointer to an
+ * int & updates its value to 98
+ *
+ * @n: integer
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+}
+
