7f8cfd37730297bcfc546eba9cab0220757b1fae
evanceondego <ondegoevance@gmail.com>
2023-04-07 02:21:05 -0700

    3-set_bit.c2

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,10 +7,13 @@
  * @n: pointer to unsigned long interger
  * @index: bit identifier
  *
- * Return: Success(1), else (0)
+ * Return: Success(1), else (-1)
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	if (index > sizeof(unsigned long int) * 8
+
+			return (-1);
 	*n = *n | (1 << index);
 	return (1);
 }
