f35f15fcf1bde4bdf8848ce7179e890226224a15
evanceondego <ondegoevance@gmail.com>
2023-06-16 01:36:46 -0700

    deletenode

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,8 +1,30 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - deletes the node at the given index
- * in a doubly linked list
+ * dlistint_len - returns the number of nodes in a doubly linked list
+ * @h: pointer to the list
+ *
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	if (!h)
+		return (0);
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
+
+/**
+ * delete_dnodeint_at_index - deltes a node in a doubly linked list
+ * at a given index
  * @head: double pointer to the list
  * @index: index of the node to delete
  *
@@ -10,33 +32,32 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *temp, *prev;
-	unsigned int i;
+	dlistint_t *temp = *head;
+	unsigned int i = 0;
 
-	if (*head == NULL || index >= dlistint_len(*head))
+	if (*head == NULL || dlistint_len(temp) < index + 1)
 		return (-1);
 
-	temp = *head;
-
-	if (index == 0)
+	if (!index)
 	{
-		*head = temp->next;
-		if (temp->next != NULL)
+		(*head) = temp->next;
+		if (temp->next)
 			temp->next->prev = NULL;
+		temp->next = NULL;
 		free(temp);
 		return (1);
 	}
 
-	for (i = 0; i < index; i++)
+	while (i < index)
 	{
-		prev = temp;
 		temp = temp->next;
+		i++;
 	}
 
-	prev->next = temp->next;
-	if (temp->next != NULL)
-		temp->next->prev = prev;
-
+	temp->prev->next = temp->next;
+	if (temp->next)
+		temp->next->prev = temp->prev;
 	free(temp);
+
 	return (1);
 }
