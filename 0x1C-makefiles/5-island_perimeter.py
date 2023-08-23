d579ffe230c3e0333a642b5903efaa12a266df0e
evanceondego <ondegoevance@gmail.com>
2023-08-23 10:34:40 -0700

    grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
-"""Module that calculates the perimeter of an island in a grid."""
-
-def num_water_neighbors(grid, i, j):
-"""Returns the number of water neighbors a cell has in a grid."""
-
-num = 0
-
-if i <= 0 or not grid[i - 1][j]:
-num += 1
-if j <= 0 or not grid[i][j - 1]:
-num += 1
-if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
-num += 1
-if i >= len(grid) - 1 or not grid[i + 1][j]:
-num += 1
-
-return num
 
 def island_perimeter(grid):
-"""Returns the perimeter of the island in grid."""
+"""
+Calculate the perimeter of the island described in the grid.
+
+Args:
+grid (List[List[int]]): The grid representing the island.
+
+Returns:
+int: The perimeter of the island.
+"""
 perimeter = 0
-for i in range(len(grid)):
-for j in range(len(grid[i])):
-if grid[i][j]:
-perimeter += num_water_neighbors(grid, i, j)
+
+rows = len(grid)
+cols = len(grid[0]) if rows > 0 else 0
+
+for row in range(rows):
+for col in range(cols):
+if grid[row][col] == 1:
+perimeter += 4  # Start with assuming all sides are exposed
+
+#Check neighboring cells and reduce perimeter for adjacent land
+if row > 0 and grid[row - 1][col] == 1:
+perimeter -= 1
+if row < rows - 1 and grid[row + 1][col] == 1:
+perimeter -= 1
+if col > 0 and grid[row][col - 1] == 1:
+perimeter -= 1
+if col < cols - 1 and grid[row][col + 1] == 1:
+perimeter -= 1
 
 return perimeter
